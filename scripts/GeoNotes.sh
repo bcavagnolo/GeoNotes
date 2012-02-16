2541e22a5a211901c6c76077e2a414a3a4341785
Brian Cavagnolo <bcavagnolo@berkeley.edu>
2012-02-16 13:12:21 -0800

    add lauch script

---
+++ scripts/GeoNotes.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# based on example code from http://www.epilogue.org/~xef4/start-stop-example
+
+set -e
+
+cd `dirname $0`/..
+DAEMON_DIR=$PWD
+
+NAME=GeoNotes
+PIDFILE=/tmp/$NAME.pid
+DAEMON=/usr/local/bin/node
+DAEMON_OPTS=./app.js
+
+
+export PATH="${PATH:+$PATH:}/usr/sbin:/sbin"
+
+case "$1" in
+  start)
+        echo -n "Starting daemon: "$NAME
+        start-stop-daemon --quiet --start -b -d $DAEMON_DIR -m --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_OPTS
+        echo "."
+        ;;
+  stop)
+        echo -n "Stopping daemon: "$NAME
+        start-stop-daemon --quiet --stop --oknodo --pidfile $PIDFILE
+        echo "."
+        ;;
+  restart)
+        echo -n "Restarting daemon: "$NAME
+		$0 stop > /dev/null
+		$0 start > /dev/null
+        echo "."
+        ;;
+
+  *)
+        echo "Usage: "$1" {start|stop|restart}"
+        exit 1
+esac
+
+exit 0
