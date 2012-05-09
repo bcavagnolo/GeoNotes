c650f15778de7f06dc95238496a9c877119bea50
Brian Cavagnolo <bcavagnolo@berkeley.edu>
2012-05-09 14:20:48 -0700

    add user controls to client UI
    This is just a start.  We still need a registration form.

--- static/javascripts/geonotes.js
+++ static/javascripts/geonotes.js
@@ -8,9 +8,6 @@ var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', s
 var formatter = new OpenLayers.Format.GeoJSON();
 var selectControl;
 
-// This is the state for the broadcast management
-var socket;
-
 // A GeoNote is just a decorated vector feature
 GeoNote = OpenLayers.Class(OpenLayers.Feature.Vector, {
 
@@ -48,7 +45,6 @@ GeoNote = OpenLayers.Class(OpenLayers.Feature.Vector, {
   announce: function() {
     point = new OpenLayers.Geometry.Point(this.lon, this.lat);
     gjson = formatter.write(point);
-    socket.emit('new geonote', gjson);
   }
 });
 
@@ -80,6 +76,83 @@ OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
     (new GeoNote(lonlat)).announce();
   }
 });
+
+// This is the user controller state machine.  It should all be wrapped up in a
+// class.  Oh well.
+UCStates = {
+  LOGGED_OUT:0,
+  LOGGING_IN:1,
+  LOGGED_IN:2,
+  AUTHENTICATING:3,
+}
+
+UCState = UCStates.LOGGED_OUT;
+var authTimer = null;
+
+function uc_set_logged_in() {
+  $('#uc_login').hide();
+  $('#uc_logged_in').show();
+  $('#uc_logged_out').hide();
+  $("#uc_login_status").text("");
+  if (authTimer) clearTimeout(authTimer);
+}
+
+function uc_set_logged_out() {
+  $('#uc_login').hide();
+  $('#uc_logged_in').hide();
+  $('#uc_logged_out').show();
+  $("#uc_login_status").text("")
+  if (authTimer) clearTimeout(authTimer);
+}
+
+function uc(e) {
+  console.log("User Controller State: " + UCState)
+  console.log("User Controller Event: " + JSON.stringify(e))
+
+  switch (UCState) {
+  case UCStates.LOGGED_OUT:
+    if (e["event"] == "init") {
+      console.log("user controller initialized");
+    } else if (e["event"] == "login") {
+      $('#uc_login').show();
+      UCState = UCStates.LOGGING_IN;
+    }
+    break;
+
+  case UCStates.LOGGING_IN:
+    if (e["event"] == "login_submit") {
+      // TODO: Here we have a dummy function used to test the authentication
+      // UI.  Ultimately, it will perform a network authentication and either
+      // succeed, fail, or timeout.
+      $("#uc_login_status").text("Authenticating...")
+      authTimer = setTimeout(function() {uc({"event":"auth_timeout"})}, 3000);
+      UCState = UCStates.AUTHENTICATING;
+    } else if (e["event"] == "login_cancel") {
+      uc_set_logged_out();
+      UCState = UCStates.LOGGED_OUT;
+    }
+    break;
+
+  case UCStates.AUTHENTICATING:
+    if (e["event"] == "auth_timeout") {
+      $("#uc_login_status").text("ERROR: Timed out while authenticating.")
+      UCState = UCStates.LOGGING_IN;
+    } else if (e["event"] == "login_cancel") {
+      uc_set_logged_out();
+      UCState = UCStates.LOGGED_OUT;
+    }
+    break;
+
+  case UCStates.LOGGED_IN:
+    if (e["event"] == "logout") {
+      uc_set_logged_out();
+      UCState = UCStates.LOGGED_OUT;
+    }
+    break;
+  default:
+    console.log("ERROR: Unexpected user controller state: " + uc_state)
+  }
+}
 
 $(document).ready(function() {
 
@@ -135,10 +208,14 @@ $(document).ready(function() {
     },
   });
 
-  // socket = io.connect();
-  // socket.on('new geonote', function (data) {
-  //   p = formatter.read(data, "Geometry");
-  //   new GeoNote(new OpenLayers.LonLat(p.x, p.y));
-  // });
+  // Set up user controller handlers
+  $("a").filter(".uc").each(function(index) {
+    $(this).click(function() {
+      uc({"event":$(this).attr('id')});
+    });
+  });
+
+  // initialize the uc controller
+  uc({"event":"init"});
 
 });
